782bb1d7ddb9367efb8a4fd529c0b71d6f02e26e
ramos-adrian <adrian@ramos.ar>
2024-09-29 10:53:51 -0300

    Move NavBar to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import "./globals.css";
 import {poppins} from "@/app/ui/fonts";
+import Navbar from "@/app/ui/Navbar";
+import React from "react";
 
-export default function RootLayout({children,}: Readonly<{children: React.ReactNode}>) {
-  return (
-    <html lang="en">
-      <body className={`${poppins.className} antialiased`}>
+export default function RootLayout({children,}: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en">
+        <body className={`${poppins.className} antialiased`}>
+        <Navbar/>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,12 @@
 
 import {APIProvider, Map} from "@vis.gl/react-google-maps";
 import React from "react";
-import Navbar from "@/app/ui/Navbar";
 
 type Poi = { key: string, location: google.maps.LatLngLiteral }
 
 export default function Home() {
     return (
         <main>
-            <Navbar/>
             <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
                 <Map
                     style={{width: '70vw', height: '70vh'}}
@@ -20,7 +18,6 @@ export default function Home() {
                     mapId={'150ff8be8393ef51'}
                 >
                 </Map>
-
             </APIProvider>
         </main>
     );
