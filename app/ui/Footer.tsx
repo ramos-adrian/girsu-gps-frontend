2df48d3bfe6e73a511dd5627eaa1881cc50395ea
ramos-adrian <adrian@ramos.ar>
2024-09-30 09:14:47 -0300

    Add "Developed By" to Footer

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -6,7 +6,7 @@ import {facebookLink, instagramLink, websiteLink, twitterLink, youtubeLink} from
 
 const Footer: React.FC = () => {
     return (
-        <footer className="bg-[#647197] text-white py-6 mt-10">
+        <footer className="bg-[#647197] text-white pt-6 mt-10">
             <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-8">
                 {/* Left side image */}
                 <div className="flex-shrink-0 mb-4 md:mb-0">
@@ -14,7 +14,8 @@ const Footer: React.FC = () => {
                 </div>
 
                 {/* Right side text */}
-                <div className="text-xs md:text-base text-center flex flex-col md:flex-col md:text-right font-normal md:font-semibold leading-loose">
+                <div
+                    className="text-xs md:text-base text-center flex flex-col md:flex-col md:text-right font-normal md:font-semibold leading-loose">
                     <div className="mb-2 flex flex-row space-x-3 justify-evenly md:justify-end items-center">
                         <div>Seguinos en</div>
                         <a href={facebookLink}><FontAwesomeIcon icon={faFacebook} height="1.2em"/></a>
@@ -22,14 +23,17 @@ const Footer: React.FC = () => {
                         <a href={twitterLink}><FontAwesomeIcon icon={faTwitter} height="1.2em"/></a>
                         <a href={youtubeLink}><FontAwesomeIcon icon={faYoutube} height="1.2em"/></a>
                         <a href={websiteLink}>
-                            <Image src="/logoMuni-sm.png" alt="Logo Muni" width={22} height={22} />
+                            <Image src="/logoMuni-sm.png" alt="Logo Muni" width={22} height={22}/>
                         </a>
                     </div>
                     <p>Municipalidad de San Miguel de Tucumán</p>
                     <p>9 de Julio 598, Tucumán. República Argentina</p>
                     <p>Municipalidad: 0381-4516500</p>
-                    <p>Asistencia Pública: 107</p>
                 </div>
+            </div>
+
+            <div className="bg-[#4a5568] text-right py-2 mt-4">
+                <p className="mr-8 text-xs md:text-base">Desarrollado por Universidad Tecnológica Nacional - Facultad Regional Tucumán - 2024</p>
             </div>
         </footer>
     );
