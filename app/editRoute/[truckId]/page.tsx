81dfc1676c567b0d38b25e4053c6600af223fbf4
ramos-adrian <adrian@ramos.ar>
2024-12-01 18:36:38 -0300

    Minor bug fix

--- app/editRoute/[truckId]/page.tsx
+++ app/editRoute/[truckId]/page.tsx
@@ -25,7 +25,6 @@ export default function Page({params}: { params: { truckId: string } }) {
                 return response.json();
             })
             .then(data => {
-                console.log(data)
                 setTruck(data);
             })
             .catch(error => {
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import {z} from "zod";
 import {useForm} from "react-hook-form";
