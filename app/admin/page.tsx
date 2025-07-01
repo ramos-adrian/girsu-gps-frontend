fbd21485540add3a8d3ae4ac36be71880ac6bcdc
Adrián Ramos <adrian@ramos.ar>
2025-06-30 23:37:36 -0300

    DEBUG: Clear notifications btn

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,28 @@
 import { DataTable } from '@/app/admin/data-table';
 import { columns } from '@/app/admin/columns';
 import {useEffect, useState} from "react";
+import {Button} from "@/components/ui/button";
+import {publicApiBaseURL} from "@/app/config";
+
+const clearNotifications = () => {
+    fetch(`${publicApiBaseURL}/user/resetAllNotifications`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+        },
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            alert("Notificaciones eliminadas correctamente");
+        })
+        .catch(error => {
+            console.error('Error clearing notifications:', error);
+            alert("Error al eliminar las notificaciones");
+        });
+}
 
 export default function TruckTable() {
 
@@ -21,6 +43,11 @@ export default function TruckTable() {
         <main className="container mx-auto flex flex-col px-5">
             <h1 className="text-2xl">Camiones</h1>
             <DataTable columns={columns} />
+            <div>
+                <Button onClick={clearNotifications} variant={"ghost"}>
+                    Clear Notifications
+                </Button>
+            </div>
         </main>
     );
 }
